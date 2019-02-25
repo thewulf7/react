faa14a2e7b2a35c3d057d030dfa3d270a51afe13
Oleksandr Fediashov <olfedias@microsoft.com>
2019-02-25 19:30:53 +0200

    fix config

--- build/jest/jest.config.strict.js
+++ build/jest/jest.config.strict.js
@@ -1,5 +1,5 @@
 const commonConfig = require('./jest.config.common')
 
 module.exports = Object.assign({}, commonConfig, {
-  setupTestFrameworkScriptFile: `${__dirname}/setup.strict.ts`,
+  setupFilesAfterEnv: [`${__dirname}/setup.strict.ts`],
 })
